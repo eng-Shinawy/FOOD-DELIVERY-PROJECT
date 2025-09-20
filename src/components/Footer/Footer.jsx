d6bf3a501c86ec8e04b07512518560aa09c3a53f
eng-shinawy <elshinawy83824964mo@gmail.com>
2025-09-20 11:05:45 +0300

    first part of frontend

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import { assets } from '../../assets/assets'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus magnam illum similique, vel cum ut quia odio quidem illo earum perferendis voluptatem, commodi quibusdam ipsa ullam veritatis est nihil laborum?</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-212-465-8975</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr/>
+      <p className="footer-copyright">Copyright 2025 © Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {BrowserRouter} from 'react-router-dom'
+import StoreContextProvider from './Context/StoreContext.jsx'
+// Get the root element from your HTML
+const rootElement = document.getElementById('root')
+
+// Create a root and render your app
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <BrowserRouter>
+      
+      <StoreContextProvider>
+          <App />
+      </StoreContextProvider>
+    </BrowserRouter>
+  )
+} else {
+  console.error('Root element not found')
+}
